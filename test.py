7b3678a4dfc5c6af08ee8f8fdf4f284461a0d763
Omena0 <Omena0MC@gmail.com>
2024-09-04 10:52:02 +0300

    a

--- test.py
+++ test.py
@@ -1,23 +1,6 @@
 import accountManager as acc
-from threading import Thread
 
 acc.load()
 
-def create(app):
-    for user in range(20):
-        acc.create(str(app),str(user),createLicence=True,createApp=True)
-        print(f'Created user {user} in app {app}')
-    acc.load(app)
 
-t = []
-for app in range(20):
-    _t = Thread(target=create,args=[app])
-    t.append(_t)
-    _t.start()
 
-for i in t:
-    i.join()
-
-for app in range(20):
-    for user in range(20):
-        print(f'Status for User {user} in app {app}: {acc.check(str(app),str(user))}')
